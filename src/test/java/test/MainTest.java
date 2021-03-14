6cc76d10b9159c0246f67ddea51cf8fbe92c17b8
buechth1 <buechth1@students.zhaw.ch>
2021-03-14 13:27:21 +0100

    test multiply

---
+++ src/test/java/test/MainTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class MainTest {
+
+
+    @Test
+    public void testMultiply(){
+        assertEquals(20, Main.testMultiply(4,5));
+    }
+
+}
